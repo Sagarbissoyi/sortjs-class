9c778e2112fdca803442e9eac2f12e86c4ec7974
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-19 23:56:54 +0530

    Add initial implementation of sorting functionality in sort.js

---
+++ sort.js
@@ -0,0 +1,50 @@
+// sort()
+
+
+
+// let arr=["mango","apple","banana","kiwi"];
+// arr.sort();
+// console.log(arr); // ["apple", "banana", "kiwi", "mango"]
+
+
+
+
+// let arr1 = [12,100,56,233,34,9]
+// arr1.sort()
+// console.log(arr1) // [100, 12, 233, 34, 56, 9]
+
+
+
+
+// let arr =["12","100","56","233","34","9"]
+// arr.sort()
+// console.log(arr) // ["100", "12", "233", "34", "56", "9"]   
+
+
+
+
+// let arr =[12,100]
+// arr.sort((a,b) => a-b)// if the sort is in ascending order then use (a,b) => a-b and if the sort is in descending order then use (a,b) => b-a
+// console.log(arr)
+
+
+//a-b is give positive value then it change the value in small to big or acending to decending
+//b-a is give negative value then it change the value in big to small or decending to acending
+
+
+// let arr=[12,100,56,233,34,9]
+// arr.sort((a,b)=>b-a)
+// console.log(arr)
+
+
+
+let arr=[12,100,56,233,34,9]
+arr.sort((a,b)=>-100)
+console.log(arr)// 
+
+
+
+
+
+
+
