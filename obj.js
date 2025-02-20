422484e684999e93acc46012077ac1e3502f29fb
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-21 00:09:42 +0530

    Remove city property from obj and log the updated object

---
+++ obj.js
@@ -0,0 +1,19 @@
+
+let obj = {
+    name: "John",
+    age: 30,
+    city: "New York"
+}
+
+// obj.city = "USA"
+// obj["country"] = "USA"
+
+
+// delete obj.city
+delete obj["city"]
+
+
+
+
+
+console.log(obj)
