0d5128c162a08bc0edff59e9fbc1cdd628b16cd3
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-21 00:28:06 +0530

    Add sorting functionality for band names with custom character removal

---
+++ sortQs.js
@@ -0,0 +1,47 @@
+const bands = ['The Plot in You', 
+    'The Devil Wears Prada', 'Pierce the Veil',
+     'Norma Jean', 'The Bled', 'Say Anything', 
+     'The Midway State', 'We Came as Romans', 
+     'Counterparts', 'Oh, Sleeper', 
+     'A Skylit Drive', 'Anywhere But Here', 
+     'An Old Dog'];
+
+// bands.sort();
+// console.log(bands);
+
+
+function removeChar(str){
+    str = " "+str + " "
+    str = str.toLowerCase();
+     str = str.replaceAll(" the "," ")
+     srt = str.replaceAll(" a ","  ")
+     str = str.replaceAll(" an ","")
+     return str.trim()
+}
+
+
+bands.sort((a,b)=>{
+    
+    if (removeChar(a) > removeChar(b)){
+        return 1
+    } else {
+        return -1
+    }
+
+})
+
+
+
+
+console.log(bands)
+
+
+
+
+
+
+
+
+
+
+
