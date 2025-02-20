a8bf0b5e4f46347c3e74047615585fb916539a8a
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-21 00:40:32 +0530

    Add find() examples for array and string searches in find.js

---
+++ find.js
@@ -0,0 +1,46 @@
+
+
+// let arr = [10,20,30,40,50]
+
+// check if 30 is present in the array
+
+// find()
+
+
+// let x =arr.find((value,index)=>
+// value == 30
+// )
+
+
+// console.log(x)
+
+
+// const users = [
+//         { id: 1, name: 'John Doe' },
+//         { id: 2, name: 'Jane Smith' },
+//         { id: 3, name: 'Emily Johnson' }
+//        ];
+
+// let user = users.find(value => value.id ==2)
+// console.log(user)
+
+
+
+// let arr1 = [10,20,30,40,50] ;
+// // find the first value > 30
+
+// let x = arr1.find(value => value > 30)
+
+// console.log(x)
+
+
+
+let strArr = ['apple', 'banana', 'mango', 'orange', 'grapes', "lalo"]
+
+let x = strArr.find(value => value.endsWith("o"))
+
+console.log(x)
+
+
+
+
